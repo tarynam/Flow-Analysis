4775e763de27c9f5f2e09e029ebd906fdd5e7bbf
tarynam <taryn.am@gmail.com>
2019-06-10 15:53:32 -0400

    boolean data gates added for markers and cytokines

--- ICS-processing.R
+++ ICS-processing.R
@@ -9,22 +9,6 @@ for(i in 1:length(filenames)){
 
 l.df<-lapply(ls(pattern=".csv"), function(x) get(x))
 combined<-dplyr::bind_rows(l.df)
-
-#this has to be edited still 
-combined$Sample<-paste(combined$donor, combined$Stim, sep="_")
-remove<-c("NK2126_HC_X_SEA", 
-          "NK2450_HC_SM_SEA" , 
-          "NK2402_LTBI_SM_SWAP",
-          "NK2342_TB_X_SWAP",
-          "NK2136_TB_X_WCL",
-          "NK2168_HC_SM_WCL",
-          "NK2421_LTBI_SM_PEP",
-          "NK2063_TB_X_SEA",
-          "NK2115_LTBI_X_PEP",
-          "NK2115_LTBI_X_WCL",
-          "NK2171_HC_X_WCL",
-          "NK2162_HC_X_SEA")
-combined<-dplyr::filter(combined, !(Sample %in% remove))
 write.csv(combined, "/Users/tarynam/Desktop/ICS-CD4/clean-data/Master-ICS.csv")
 
 
