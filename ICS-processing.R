9ba2c2c7542bc408f437b73b85d42fbc971e3fd8
tarynam <taryn.am@gmail.com>
2019-08-13 19:00:03 -0400

    added in line to fix 2054 export

--- ICS-processing.R
+++ ICS-processing.R
@@ -7,8 +7,9 @@ for(i in 1:length(filenames)){
     assign(filenames[i], read.table(filenames[i], header=TRUE, sep=";"))
 }
 
+NK2054_TB_SM.csv$donor<-"NK2054_TB_SM"
+
 l.df<-lapply(ls(pattern=".csv"), function(x) get(x))
 combined<-dplyr::bind_rows(l.df)
-write.csv(combined, "/Applications/Old Computer/Day Lab/Flow-Data/ICS-CD4/Master-ICS.csv")
-
+write.csv(combined, "/Applications/Old Computer/Day Lab/Flow-Data/Master-ICS.csv")
 
