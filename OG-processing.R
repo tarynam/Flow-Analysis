f9d3ed6dced308d1e8c1832cdb265fe8498cf6fd
tarynam <taryn.am@gmail.com>
2019-11-27 13:38:11 -0800

    cleaned up HD

--- OG-processing.R
+++ OG-processing.R
@@ -1,16 +1,14 @@
 library(dplyr)
 library(data.table)
 
-setwd("/Applications/Old Computer/Day Lab/Flow-Data/ICS-CD4")
+setwd("/Applications/Old Computer/Day Lab/Flow-Data/oregon-green")
 filenames = list.files(pattern="csv")
 for(i in 1:length(filenames)){
     print(filenames[i])
     assign(filenames[i], read.table(filenames[i], header=TRUE, sep=";"))
 }
 
-NK2054_TB_SM.csv$donor<-"NK2054_TB_SM"
-
 l.df<-lapply(ls(pattern=".csv"), function(x) get(x))
 combined<-dplyr::bind_rows(l.df)
-fwrite(combined, "/Applications/Old Computer/Day Lab/Flow-Data/Master-ICS.csv")
+fwrite(combined, file="/Applications/Old Computer/Day Lab/Flow-Data/Master-OG2.csv")
 
